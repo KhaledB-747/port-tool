ec1fc8e06a4f46a92f3ba89161d2d99bfdf691ba
KhaledB-747 <kh3bns@gmail.com>
2026-01-30 11:25:31 -0500

    version 3

--- portinfo.py
+++ portinfo.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python
+#this tools create by Khaled, Gmail: kh3bns@gmail.com
 import socket
 import argparse 
 import pandas as pa
 from colorama import Fore,Style 
-#this tools create by Khaled, Gmail: kh3bns@gmail.com 
+ope = 0
+clo = 0
+unk = 0
 def check_port(port):
+    global ope, clo, unk
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(1)
         result = s.connect_ex(("127.0.0.1", port))
         s.close()
         if result == 0:
-            return ' Open'
+            ope += 1
+            return 'Open'
         else:
-            return ' Closed'
+            clo += 1
+            return 'Closed'
     except Exception:
+        unk += 1
         return 'Unknown'
 arg = argparse.ArgumentParser()
 arg.add_argument("-m","--number", type=int, help="Get service name by port number")
@@ -26,6 +33,9 @@ prot_name = args.name
 port_number = args.number
 start_range = args.s
 end_range = args.e
+count1 = 0
+count2 = 0
+total = 0
 if not any(vars(args).values()):
     arg.print_help()
     exit()
@@ -38,10 +48,14 @@ try:
                 result["Port"].append(i)
                 result["Name"].append(name)
                 result["Status"].append(check_port(i))
+                count1 += 1
             except (OSError, socket.error):
-                pass
+                count2 += 1
         pr = pa.DataFrame(result)
         print(Fore.YELLOW + pr.to_string(index=False) + Style.RESET_ALL)
+        print(f"{count1+count2} ports has been scaned between {start_range}-{end_range}")
+        print(f"Existing: {count1}, not existing: {count2}")
+        print(f"Open: {ope}, Closed: {clo}, Unknown: {unk}")
     elif port_number:
         try:
             name = socket.getservbyport(port_number)
@@ -59,6 +73,5 @@ try:
         except Exception:
             print(f"Port with name '{prot_name}' does not exist")
 except Exception as d:
-    print(d)
     print(Fore.RED+"Unknown error!"+Style.RESET_ALL)
     print(Fore.RED+"Use --help to more information"+Style.RESET_ALL)
